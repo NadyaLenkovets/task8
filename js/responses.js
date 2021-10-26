07463033861f99386079753800e1d0e9c5ae5611
NadyaLenkovets <nadya.lenkovets@gmail.com>
2021-10-26 15:49:25 +0300

    add files

---
+++ js/responses.js
@@ -0,0 +1,74 @@
+function getBotResponse(input) {
+   if (input.toLowerCase() == 'расскажи шутку.') {
+      return tellAJoke();
+   } else if (input.toLowerCase() == 'какой сегодня день?') {
+      return getDay();
+   } else if (input.toLowerCase() == 'сколько дней осталось до нового года?') {
+      return daysLeftToNewYear();
+   } else if (input.toLowerCase() == 'скинь мемасик).') {
+      return '<img class="bot-image" src="img/image.jpg">';
+   } else if (input.toLowerCase() == 'хочу записаться на стрижку к алине.') {
+      return 'В какой день и время вам будет удобно?';
+   } else if (input.toLowerCase() == 'в субботу в 10:30.') {
+      return `Вы записаны на стрижку к мастеру Алине в субботу в 10:30.`;
+   } else if (input.toLowerCase() == 'Пока!') {
+      return `До свидания!`;
+   } else if (input == 'Мне нравится этот бот)).') {
+      return 'Спасибо!))';
+   } else {
+      return 'Я вас не понимаю.'
+   }
+} 
+
+
+function getDay() {
+   let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+   let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+   let today = new Date();
+   
+   let day = today.getDay();
+   let date = today.getDate();
+   let month = today.getMonth();
+
+   return days[day] + ' , ' + date + ' ' + months[month] + '.';
+}
+
+
+function daysLeftToNewYear() {
+   let now = new Date();
+   let newYear = new Date(now.getFullYear(), 11, 31);
+
+   let diff = newYear - now; // разница
+
+   if (diff > 0) {
+      return 'До Нового года осталось ' + Math.round(diff / (1000 * 60 * 60 * 24)) + ' дней.';
+   } else if (diff == 0) {
+      return 'Новый год сегодня!';
+   }
+}
+
+
+function tellAJoke() {
+   let jokes = [`"Красиво сделайте" -
+                  ТЗ, которое мы заслужили.`,
+                `Ревность - когда таксист берёт
+                 новый заказ, но ещё не довёз тебя.`,
+                `Страшилка: однажды Лиля не сфотографировала, 
+                 что летит бизнес-классом и все подумали,
+                  что она бедная.`, 
+                `Торговый комплекс - когда тебе неудобно торговаться.`,
+                `Воскнедельник - момент, когда воскресенье
+                 перестает ощущаться как воскресенье и начинается 
+                 понедельничное беспокойство.`,
+                 `Печатает... - то, что ты видишь 10 минут
+                 перед тем, как тебе напишут "ок".`,
+               ];
+   let counter = getRandomInt(0, 5);
+
+   function getRandomInt(min, max) {
+      return Math.floor(Math.random()*(max-min+1)) + min;
+   }
+
+   return jokes[counter];
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let chat = document.querySelector('.chat');
+
 // create date
 
 function getTime() {
@@ -12,9 +14,79 @@ function getTime() {
    if (minutes < 10) {
       minutes = "0" + minutes;
    }
-   return currentTime = hours + ':' + minutes;
+   return currentTime = 'Сегодня в ' + hours + ':' + minutes;
 }
 
+
+// add first bot message to chat
+
 function firstBotMessage() {
-   let firstMessage = 'Привет! Как дела?'
+   let firstMessage = 'Привет! Как дела?';
+
+   document.getElementById('bot-starter-message').innerHTML = '<p class="bot-text"><span class="bot-text__span">' + firstMessage + '</span></p>';
+
+   let time = getTime();
+   // add date to chat
+   document.getElementById('timestamp').innerHTML = time;
 }
+
+firstBotMessage();
+
+
+
+function getHardResponse(userText) {
+   
+   let botResponse = getBotResponse(userText);
+
+    // create bot response with loader first
+   let botHTML = document.createElement('p');
+   botHTML.classList.add('bot-text');
+   botHTML.innerHTML = '<span class="loader-block"><span class="loader loader--left"></span><span class="loader loader--center"></span><span class="loader loader--right"></span></span>';
+
+   // add bot loader to chat
+   chat.appendChild(botHTML);
+
+   setTimeout(() => {
+      showBotAnswer();
+   }, 2500);
+
+   function showBotAnswer() {
+      // create bot response with words
+      botHTML.innerHTML = '<span class="bot-text__span">' + botResponse + '</span>';
+   }
+}
+
+
+function getResponse() {
+   let userText = document.querySelector('.text-input');
+   let userTextValue = document.querySelector('.text-input').value;
+
+   if (userText.value == '') {
+      userText.value = 'Мне нравится этот бот)).';
+      userTextValue = 'Мне нравится этот бот)).';
+   }
+
+   // create user response
+   let userHTML = document.createElement('p');
+   userHTML.classList.add('user-text');
+   userHTML.innerHTML = '<span>' + userText.value + '</span>';
+
+   // add user response to chat
+   chat.appendChild(userHTML);
+
+   // clear user input
+   userText.value = '';
+
+   // show bot response
+   setTimeout(() => {
+      getHardResponse(userTextValue);
+   }, 500);
+}
+
+// press Enter to send user message
+document.querySelector('.text-input').addEventListener('keydown', function(event) {
+   if (event.code == 'Enter' || event.code == 'NumpadEnter') {
+      getResponse();
+   }
+})
+
